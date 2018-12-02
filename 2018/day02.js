e9c195b98c095e856fd11b8121325b9be55834d2
Rolf Schäuble <rolf@rschaeuble.de>
2018-12-02 09:14:54 +0100

    AoC 2018, day 2.

---
+++ 2018/day02.js
@@ -0,0 +1,346 @@
+const input = `
+bpacnmelhhzpygfsjoxtvkwuor
+biacnmelnizqygfsjoctvkwudr
+bpaccmllhizyygfsjoxtvkwudr
+rpacnmelhizqsufsjoxtvkwudr
+bfacnmelhizqygfsjoxtvwwudp
+bpacnmelhizqynfsjodtvkyudr
+bpafnmelhizqpgfsjjxtvkwudr
+bpackmelhizcygfsjoxtvkwudo
+bmacnmilhizqygfsjoltvkwudr
+bpafnmelhizuygfsjoxtvkwsdr
+boacnmylhizqygfsjoxtvxwudr
+bpbcjmelhizqygfsjoxtgkwudr
+bpacnmglhizqygfsjixtlkwudr
+bpacnmclhizqygfsjoxtvkwtqr
+bpacnmelhczqygtsjoptvkwudr
+bpacnmelhizqywfsaoxtvkbudr
+apacnmelhizqygcsjoxtvkwhdr
+bpacnmelrizqygfsbpxtvkwudr
+tpkcnmelpizqygfsjoxtvkwudr
+bpacnmelhizqlgfsjobtmkwudr
+npacnmelhizqygffjoxtvkwudf
+bpacnmeehqzqygqsjoxtvkwudr
+bpecnmelhizqigfsjvxtvkwudr
+bpacnmelhizqysfsjoxtvkdfdr
+bpacnfelhkzqygfsjoxtvkwfdr
+bpacnbelvizqygfsjoxthkwudr
+bpacnoelhizqygfejoxtvkwudn
+bpacnmelhizqygfzpkxtvkwudr
+bpahnmelhizqyufsjoxmvkwudr
+bpacnmelhizqygfsnoxtvkwmmr
+bpacnmelhizqygfsjoatvkludf
+bpacnmylhizqygfsjlxtvksudr
+bpacnmekhpzqygysjoxtvkwudr
+bpacnselhizqogfswoxtvkwudr
+bpacnmelhizqprfsjoxwvkwudr
+bpatnmelhinqygfsjoctvkwudr
+bpacnqelhqzqygfsxoxtvkwudr
+bpabnmelhiyqygfsjoxtykwudr
+bpacnivlhizqygfsjoxtviwudr
+bpkcnmylhizqygfsjoxtvkwcdr
+bpafnmflhizqygtsjoxtvkwudr
+bpachmelhizqygfsjixtvkwudg
+bpacymelhizqygfsjoxtykwuar
+bpacnkelhizqdgfsjoxtskwudr
+bpacnmezhizqggbsjoxtvkwudr
+bpacnmqlhizqygrsjoxzvkwudr
+bpaczmelhizqyhfsjoxfvkwudr
+bdacnmelhyzqygusjoxtvkwudr
+bpacbmelhizqywfsjostvkwudr
+bpacnmelhihzygfstoxtvkwudr
+bpactmelhizqygfsjcxtvkwydr
+bkacnmethizqytfsjoxtvkwudr
+bpacnmalhizqydfskoxtvkwudr
+spacnmelbizqygfsjoxdvkwudr
+lpalnmelhizoygfsjoxtvkwudr
+bpacjmeghizqygfsjoxtviwudr
+bpacnmeqhizxygfsjoxgvkwudr
+bpacnmelhizqygosjoxtvkkuhr
+bpacnmelhiznbxfsjoxtvkwudr
+bgacnmelhizqygfsjbxivkwudr
+bpacnmelhizqygfjjowtvswudr
+bpacnmelhizqygfsjovtgkmudr
+bpacnmelcmzqygfspoxtvkwudr
+bpvcnmelhizqyvfcjoxtvkwudr
+bpacnmeahizqjgfsjoxtvkwukr
+bpacnoelwizqygfsjoxtvkaudr
+xpacnmelhizqygfsjoxdvkwedr
+mpacnmelqizqygfsjoxtvkwudx
+bppcnmelhizqygfsjfxtvkhudr
+bpacnmclhizqyhfsjaxtvkwudr
+opacsmelhizqygfsjmxtvkwudr
+bpafnmelhizqjgfsjoxtvkrudr
+bpdcnmilhizqygfsjoxtvkludr
+bpainmelhizqygfsjtntvkwudr
+bradnmelhizqygfsjextvkwudr
+bpacnmelhizqygfmsoxtvkwudg
+bpacneelhizqygvrjoxtvkwudr
+bpacnpelhizqygfsjoxyvkwudf
+bpacnmelhizqygfsqoqtvkwodr
+bpacnmelhizjyghsjoxcvkwudr
+bpacnmelmibqygfsjoxtvnwudr
+jpacnmelaizqygfwjoxtvkwudr
+zpachmelhizqygfsjsxtvkwudr
+bpacnmelfizqykfsjomtvkwudr
+bpacnmllwizqygfsjoxtvkwusr
+bpaynmelhizqygfsjoxtvowcdr
+jpacnmqlhizqygfsjoxtvknudr
+bpacxmelhizqyffsjoxtvkwugr
+apawnmelhizqygfsjtxtvkwudr
+mpacnmelhitqigfsjoxtvkwudr
+bpacnmelhhzqygfsjoxtvkyzdr
+gpacnmelhizqynfsjoxtvkwudm
+bnacnkelhizqygfsjoxtpkwudr
+bpacnmelfizqygfsumxtvkwudr
+bpacnmelhisqygfsjohtvowudr
+bpacnmelhimqygxsjoxtvkwudn
+bpscnmeliizqygfsjoxtvkwunr
+qpacnmelhizqycfsjoxtvkwndr
+bpacnmelhijqygfsjohtvkyudr
+bpacnmelhizqykfsjkxtvknudr
+bpacnqilhizqygfsjoxtvkoudr
+bpacnmelhizqzgmsjoxtvkwurr
+bpdcnmelhizqygfsjoutukwudr
+bpecnmeghizqygfsjoxgvkwudr
+bpicnmelhizqygfrjoxtvlwudr
+bpacnmelhizfygfsroxtvkwodr
+buacnmelhizqygjsjoxtvkvudr
+bpacnmelhixqykfsjoxtvrwudr
+bpacnmelhizqygvejcxtvkwudr
+bpacnmjlhizqylfsjoxtvkwuor
+qpacnmelhizqygfsjoxfdkwudr
+bpfcnmemhizqygfsjoxtvknudr
+bpacnmelhizqoffsjqxtvkwudr
+hpacnielhiqqygfsjoxtvkwudr
+gpacnmelhizqygfsewxtvkwudr
+bpacnmellizqylxsjoxtvkwudr
+bpacnmenhizqymfsjoxtvkmudr
+bpacnfelhizqygcsjoltvkwudr
+bpacnmelhqqqygfsjoxtvkuudr
+bplgnmelhiqqygfsjoxtvkwudr
+bpacnzelhizqygfgjoxtvnwudr
+bpacnmelhizqygfsjoktvknunr
+bpacnmdlhioqygfnjoxtvkwudr
+epacnmelwizqyjfsjoxtvkwudr
+bpacxmelhazfygfsjoxtvkwudr
+bpacnmejhezqygfsjoxtskwudr
+bpacnqelhihqyzfsjoxtvkwudr
+bpacnbelhizqyrfsjoxtvkmudr
+bpacnmelhizqygfsjoxtylwzdr
+bpacnmelwizqygfsjodtvkhudr
+bpacnnelhizqygfsjoxtwkwadr
+bpacimelhizqygfsnoxtvkwuor
+bpacnmelhizqyaasjoxtlkwudr
+bpacnmelhizqyeffjoxtvkwuds
+bpacnmenhizqygxscoxtvkwudr
+bpacnmelhidqygfsjowtskwudr
+bpacnmeliizqygfsjoxhvkwucr
+bpacimelhizqygfsjoxtvktuwr
+bpainmelhhzqygfsjzxtvkwudr
+bpacamelhizqygfsjogtvkwbdr
+bpccnmelgizqygfsjoxtykwudr
+bpacnmelhizwegfsjoxtvkwadr
+bpackmelhbzqygqsjoxtvkwudr
+bpacymeihizqyffsjoxtvkwudr
+bpacnielhczqygfsjoxtvkwudk
+bpacnmejhizqygffjoxjvkwudr
+ppacnmelhizqygfsjoxtigwudr
+bpjcnmolhizqygfsjoxtvkwndr
+bpacnmelcizqygrsjoxtakwudr
+cpawnmelhizqygfsjoxmvkwudr
+bwacnmelhizqygesjoxtakwudr
+bpacnmelhizqygfsjexsvkwddr
+bpaunmelhiuqygfsjoxtvkwtdr
+bpacnmellimqygfsjextvkwudr
+bpacnmerhizqygfsaoxvvkwudr
+bpacnmglhizqygfsjixtukwudr
+ppacnmelhizqygfsjoxtvkdudp
+bpacnmedhizqygukjoxtvkwudr
+bpccnmelhizqngfsjoxtvkwadr
+bgacnmeldizqygfscoxtvkwudr
+bpacngelhizsygfsjoxtvkwkdr
+bpacnpelhizqygfsjoxctkwudr
+bpacnmylhizqygfcjoxtvkwmdr
+npacnmelhizqygfsjoxtwkwuds
+bpaxnmelhizqydfsjoxyvkwudr
+bpacnhelhizjygfsjoxtvkmudr
+bpacnkelhczqygfnjoxtvkwudr
+bfacnmelhizrygfsjoxtvkwodr
+bpycnmelhizqygfofoxtvkwudr
+qpacpselhizqygfsjoxtvkwudr
+bpvcnmelhezqygfsjoxttkwudr
+bpacnmwlhizqygfijoxtmkwudr
+bsacnmelhikqygfsjoxttkwudr
+bpccnxelhizqyafsjoxtvkwudr
+bpacnmelhizqygfswhxtvewudr
+vpacnmzlhizqygfsvoxtvkwudr
+bpacnmelhihqygfsjoxtvkqurr
+bpacnmelhixqygazjoxtvkwudr
+bpavnmelhizqygfsjozpvkwudr
+bpacnmclhizuygfsjoxmvkwudr
+bpacnmelhizryufsjoxtkkwudr
+bpacnmelhtzqygfsjobtvkwufr
+bpacnmelhizqmlfsjoxtvkwudq
+bpaaneelhizqygfsjlxtvkwudr
+bpacnmelhxzqygfsjoxthkwuhr
+bpacnmeshizqygfcjoxtvkwude
+bpacnzqlhizqygfsxoxtvkwudr
+bgaanmelhizqycfsjoxtvkwudr
+bpacnmexhizqygfsroxtvkwudn
+bpmmnmelhizqygfajoxtvkwudr
+bpacnmelhizqylfsjoxtckwhdr
+bpicnmelhizqyrfsjoxtvkwudi
+zpacnmelhizvycfsjoxtvkwudr
+bpamnmkllizqygfsjoxtvkwudr
+bpacnmelhrzqyrfsjoxgvkwudr
+bpadnmelhczqygfsjoxtlkwudr
+bpacrmelhizqygrsjoxtvkiudr
+lpacnmelhizqygfsjoxtgkwxdr
+fpacnmalhiuqygfsjoxtvkwudr
+bpacnmelhizqygfsjixtvfwcdr
+bpccnmelhxzqygfkjoxtvkwudr
+bpacnmepaizqygfsjoctvkwudr
+tpacnmelhivqygfsxoxtvkwudr
+kpacnfelhitqygfsjoxtvkwudr
+baacnzelhizqygfsjoxtvkwudx
+bcycnmeghizqygfsjoxtvkwudr
+wpacotelhizqygfsjoxtvkwudr
+bpacnmsshizqygrsjoxtvkwudr
+blacnmelhizqygfsjoxtykwvdr
+bkacnmelhizqygfsjoxuvkludr
+bpacnmelhizaugfsjoxtvhwudr
+fpavnmelhizqygfsgoxtvkwudr
+bpachmelnizqygfsjextvkwudr
+bpacnmelhizqpgfsjoxtvkwldu
+bpacnmelhizqygfsloftvywudr
+bpacntelhvzqygfejoxtvkwudr
+bpacnmeldizqygfsjmxtvkdudr
+byacnmelhizqygfsjsxtvkwudh
+bpacnmellizqygssxoxtvkwudr
+bpacnmelhizqygfsjootvknuir
+bpacnmelhitqjgfsjoxivkwudr
+bpacnmelhazaygfsjoxtvfwudr
+bpacnzenhizqygfsjzxtvkwudr
+bpacnmelhizqypfsdoxtvkwuar
+bpannmelhizqygnsjoxtvkwndr
+bracnmeldizsygfsjoxtvkwudr
+bpacnmelhizwygfsjugtvkwudr
+bpatnmelhizqygfsjoytvkwulr
+upacnmelhizqygfsjurtvkwudr
+bpaenmezhizqygfsjostvkwudr
+bpacnmelhizpygfsjodhvkwudr
+bpacnmelhizqygfsjogtvkguwr
+bpacnmelhisqygfsjoxtpkuudr
+bxacnmelhizqygfsjdxtvkfudr
+bpacnmelhizqygfsjohqvkwudu
+bzacnmtlhizqygfsjoxsvkwudr
+bpacnmplhixrygfsjoxtvkwudr
+bpacnmelhizqhgfsjomtvkwudg
+bpacnmezhizqygfsjxxtykwudr
+bpacnmwlhizqygfujoxtzkwudr
+tpacnmelhizqygfsjoxkvpwudr
+bpawsmenhizqygfsjoxtvkwudr
+bpacnmelhizqtgfsjoxttkwuqr
+bpkcbmelhizqygfsjoxtvkwucr
+bpacfmekhizqygfsjoxtvkwuds
+bpacnmethizqynfajoxtvkwudr
+bpocnmclhizqygfsjoxtvkwukr
+zpacnmwlhizqygfsjoxzvkwudr
+bpacpoelhqzqygfsjoxtvkwudr
+bpacnlelhizqyzfsjoxtvkwukr
+`;
+
+const ids = [];
+for (id of input.split('\n')) {
+    if (id.length !== 0) {
+        ids.push(id);
+    }
+}
+
+function countLetters(input) {
+    const letters = {};
+
+    for (c of input) {
+        letters[c] = (letters[c] | 0) + 1;
+    }
+
+    return letters;
+}
+
+function hasLetterWithOccurrences(letters, occurrences) {
+    for (key in letters) {
+        if (letters[key] === occurrences) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function calcChecksum() {
+    let idsWith2Occurrences = 0;
+    let idsWith3Occurrences = 0;
+
+    for (id of ids) {
+        if (id.length === 0) {
+            continue;
+        }
+
+        letters = countLetters(id);
+        if (hasLetterWithOccurrences(letters, 2)) {
+            ++idsWith2Occurrences
+        }
+        if (hasLetterWithOccurrences(letters, 3)) {
+            ++idsWith3Occurrences;
+        }
+    }
+    
+    console.log(`2 occurrences: ${idsWith2Occurrences}`);
+    console.log(`3 occurrences: ${idsWith3Occurrences}`);
+    console.log(`Checksum: ${idsWith2Occurrences * idsWith3Occurrences}`);
+}
+
+// Assumes a.length === b.length, which is always true in this puzzle.
+function countDifferences(a, b) {
+    let diffs = 0;
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) {
+            ++diffs;
+        }
+    }
+    return diffs;
+}
+
+function findSimilarIds() {
+    for (let i = 0; i < ids.length; ++i) {
+        for (let j = i + 1; j < ids.length; ++j) {
+            const a = ids[i];
+            const b = ids[j];
+            const diffs = countDifferences(a, b);
+            if (diffs === 1) {
+                return [a, b];
+            }
+        }
+    }
+}
+
+function findCommonLetters() {
+    let [a, b] = findSimilarIds(); // find the two IDs we are interested in
+    console.log(a,b);
+    const common = [];
+
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] === b[i]) {
+            common.push(a[i]);
+        }
+    }
+
+    return common.join('');
+}
+
+// Part 1
+calcChecksum();
+
+
+// Part 2
+console.log(`Common letters: ${findCommonLetters()}`);
